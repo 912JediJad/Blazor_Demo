138506d96d62c386f75c77e84629586fb75c1c45
“jcarri03 <jcarri03@villanova.edu>
2025-12-09 18:31:45 -0500

    homepage

--- Final_Project/obj/Debug/net10.0/Final_Project.AssemblyInfo.cs
+++ Final_Project/obj/Debug/net10.0/Final_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea56799aafd7e64474f021f3e89eaabe6fc7ff06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
